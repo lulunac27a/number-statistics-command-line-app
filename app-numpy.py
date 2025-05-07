328af481a1189747eb4298d8a8c7278a4750182b
lulunac27a <100660343+lulunac27a@users.noreply.github.com>
2025-05-07 14:25:25 -0500

    Don't print statistics if no numbers are entered

--- app-numpy.py
+++ app-numpy.py
@@ -14,25 +14,32 @@ while True:
     except ValueError:
         print("Invalid input. It will now print the statistics.")
         break  # if input is not a number, break out of loop and print statistics
-print("Minimum: ", np.min(numbers))  # minimum value
-print("First quartile: ", np.percentile(numbers, 25))  # first quartile value
-print("Median: ", np.median(numbers))  # median value
-print("Third quartile: ", np.percentile(numbers, 75))  # third quartile value
-print("Maximum: ", np.max(numbers))  # maximum value
-print("Mean: ", np.mean(numbers))  # mean value
-print("Standard deviation: ", np.std(numbers))  # standard deviation value
-while True:
-    print("Enter quartile value: ")
-    quartile_input: str = input()  # read input
-    try:
-        quartile_value = float(quartile_input)
-        if quartile_value >= 0 and quartile_value <= 1:
-            print(
-                "Quartile value: ", np.percentile(
-                    numbers, quartile_value * 100)
-            )  # print quartile value
-        else:
-            raise ValueError
-    except ValueError:
-        print("Invalid input. The program will now end.")
-        break  # end the program
+if len(numbers) == 0:
+    print("No numbers entered. The program will now end.")
+else:
+    print("Minimum: ", np.min(numbers))  # minimum value
+    print(
+        "First quartile: ", np.percentile(numbers, 25)
+    )  # first quartile value
+    print("Median: ", np.median(numbers))  # median value
+    print(
+        "Third quartile: ", np.percentile(numbers, 75)
+    )  # third quartile value
+    print("Maximum: ", np.max(numbers))  # maximum value
+    print("Mean: ", np.mean(numbers))  # mean value
+    print("Standard deviation: ", np.std(numbers))  # standard deviation value
+    while True:
+        print("Enter quartile value: ")
+        quartile_input: str = input()  # read input
+        try:
+            quartile_value = float(quartile_input)
+            if quartile_value >= 0 and quartile_value <= 1:
+                print(
+                    "Quartile value: ",
+                    np.percentile(numbers, quartile_value * 100),
+                )  # print quartile value
+            else:
+                raise ValueError
+        except ValueError:
+            print("Invalid input. The program will now end.")
+            break  # end the program
